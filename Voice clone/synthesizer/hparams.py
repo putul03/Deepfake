fe882761232503a4e8bcd1955493d263842e94b0
Putul Singh <127419636+putul03@users.noreply.github.com>
2024-06-13 18:17:36 +0530

    hparams.py

--- Voice clone/synthesizer/hparams.py
+++ Voice clone/synthesizer/hparams.py
@@ -23,17 +23,16 @@ hparams = HParams(
         sample_rate = 16000,
         n_fft = 800,
         num_mels = 80,
-        hop_size = 200,                             # Tacotron uses 12.5 ms frame shift (set to sample_rate * 0.0125)
-        win_size = 800,                             # Tacotron uses 50 ms frame length (set to sample_rate * 0.050)
+        hop_size = 200,                             
+        win_size = 800,                            
         fmin = 55,
         min_level_db = -100,
         ref_level_db = 20,
-        max_abs_value = 4.,                         # Gradient explodes if too big, premature convergence if too small.
-        preemphasis = 0.97,                         # Filter coefficient to use if preemphasize is True
+        max_abs_value = 4.,                     
+        preemphasis = 0.97,                    
         preemphasize = True,
 
-        ### Tacotron Text-to-Speech (TTS)
-        tts_embed_dims = 512,                       # Embedding dimension for the graphemes/phoneme inputs
+        tts_embed_dims = 512,                      
         tts_encoder_dims = 256,
         tts_decoder_dims = 128,
         tts_postnet_dims = 512,
@@ -43,30 +42,23 @@ hparams = HParams(
         tts_num_highways = 4,
         tts_dropout = 0.5,
         tts_cleaner_names = ["english_cleaners"],
-        tts_stop_threshold = -3.4,                  # Value below which audio generation ends.
-                                                    # For example, for a range of [-4, 4], this
-                                                    # will terminate the sequence at the first
-                                                    # frame that has all values < -3.4
+        tts_stop_threshold = -3.4,                 
+        tts_schedule = [(2,  1e-3,  20_000,  12),   
+                        (2,  5e-4,  40_000,  12),   
+                        (2,  2e-4,  80_000,  12),   
+                        (2,  1e-4, 160_000,  12),   
+                        (2,  3e-5, 320_000,  12),   
+                        (2,  1e-5, 640_000,  12)],  
 
-        ### Tacotron Training
-        tts_schedule = [(2,  1e-3,  20_000,  12),   # Progressive training schedule
-                        (2,  5e-4,  40_000,  12),   # (r, lr, step, batch_size)
-                        (2,  2e-4,  80_000,  12),   #
-                        (2,  1e-4, 160_000,  12),   # r = reduction factor (# of mel frames
-                        (2,  3e-5, 320_000,  12),   #     synthesized for each decoder iteration)
-                        (2,  1e-5, 640_000,  12)],  # lr = learning rate
-
-        tts_clip_grad_norm = 1.0,                   # clips the gradient norm to prevent explosion - set to None if not needed
-        tts_eval_interval = 500,                    # Number of steps between model evaluation (sample generation)
-                                                    # Set to -1 to generate after completing epoch, or 0 to disable
-
-        tts_eval_num_samples = 1,                   # Makes this number of samples
+        tts_clip_grad_norm = 1.0,                   
+        tts_eval_interval = 500,                   
+        tts_eval_num_samples = 1,        
 
         ### Data Preprocessing
         max_mel_frames = 900,
         rescale = True,
         rescaling_max = 0.9,
-        synthesis_batch_size = 16,                  # For vocoder preprocessing and inference.
+        synthesis_batch_size = 16,                  
 
         ### Mel Visualization and Griffin-Lim
         signal_normalization = True,
@@ -74,18 +66,18 @@ hparams = HParams(
         griffin_lim_iters = 60,
 
         ### Audio processing options
-        fmax = 7600,                                # Should not exceed (sample_rate // 2)
-        allow_clipping_in_normalization = True,     # Used when signal_normalization = True
-        clip_mels_length = True,                    # If true, discards samples exceeding max_mel_frames
-        use_lws = False,                            # "Fast spectrogram phase recovery using local weighted sums"
-        symmetric_mels = True,                      # Sets mel range to [-max_abs_value, max_abs_value] if True,
-                                                    #               and [0, max_abs_value] if False
-        trim_silence = True,                        # Use with sample_rate of 16000 for best results
+        fmax = 7600,                                
+        allow_clipping_in_normalization = True,     
+        clip_mels_length = True,                    
+        use_lws = False,                            
+        symmetric_mels = True,                      
+                                                   
+        trim_silence = True,              
 
         ### SV2TTS
-        speaker_embedding_size = 256,               # Dimension for the speaker embedding
-        silence_min_duration_split = 0.4,           # Duration in seconds of a silence for an utterance to be split
-        utterance_min_duration = 1.6,               # Duration in seconds below which utterances are discarded
+        speaker_embedding_size = 256,              
+        silence_min_duration_split = 0.4,           
+        utterance_min_duration = 1.6,               
         )
 
 def hparams_debug_string():

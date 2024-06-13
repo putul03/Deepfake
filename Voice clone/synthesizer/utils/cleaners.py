0f82cdee4f6b365e01dd95939f6cc0df1b66e428
Putul Singh <127419636+putul03@users.noreply.github.com>
2024-06-13 18:11:03 +0530

    cleaners.py

--- Voice clone/synthesizer/utils/cleaners.py
+++ Voice clone/synthesizer/utils/cleaners.py
@@ -1,23 +1,8 @@
-"""
-Cleaners are transformations that run over the input text at both training and eval time.
-
-Cleaners can be selected by passing a comma-delimited list of cleaner names as the "cleaners"
-hyperparameter. Some cleaners are English-specific. You"ll typically want to use:
-  1. "english_cleaners" for English text
-  2. "transliteration_cleaners" for non-English text that can be transliterated to ASCII using
-     the Unidecode library (https://pypi.python.org/pypi/Unidecode)
-  3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
-     the symbols in symbols.py to match your data).
-"""
 import re
 from unidecode import unidecode
 from synthesizer.utils.numbers import normalize_numbers
 
-
-# Regular expression matching whitespace:
 _whitespace_re = re.compile(r"\s+")
-
-# List of (regular expression, replacement) pairs for abbreviations:
 _abbreviations = [(re.compile("\\b%s\\." % x[0], re.IGNORECASE), x[1]) for x in [
     ("mrs", "misess"),
     ("mr", "mister"),
@@ -64,14 +49,12 @@ def convert_to_ascii(text):
 
 
 def basic_cleaners(text):
-    """Basic pipeline that lowercases and collapses whitespace without transliteration."""
     text = lowercase(text)
     text = collapse_whitespace(text)
     return text
 
 
 def transliteration_cleaners(text):
-    """Pipeline for non-English text that transliterates to ASCII."""
     text = convert_to_ascii(text)
     text = lowercase(text)
     text = collapse_whitespace(text)
@@ -79,7 +62,6 @@ def transliteration_cleaners(text):
 
 
 def english_cleaners(text):
-    """Pipeline for English text, including number and abbreviation expansion."""
     text = convert_to_ascii(text)
     text = lowercase(text)
     text = expand_numbers(text)

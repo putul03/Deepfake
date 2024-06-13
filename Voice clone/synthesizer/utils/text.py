b62591a6f13a11b853714d4f5c02511e290d065a
Putul Singh <127419636+putul03@users.noreply.github.com>
2024-06-13 18:12:44 +0530

    text.py

--- Voice clone/synthesizer/utils/text.py
+++ Voice clone/synthesizer/utils/text.py
@@ -12,18 +12,7 @@ _curly_re = re.compile(r"(.*?)\{(.+?)\}(.*)")
 
 
 def text_to_sequence(text, cleaner_names):
-    """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
 
-      The text can optionally have ARPAbet sequences enclosed in curly braces embedded
-      in it. For example, "Turn left on {HH AW1 S S T AH0 N} Street."
-
-      Args:
-        text: string to convert to a sequence
-        cleaner_names: names of the cleaner functions to run the text through
-
-      Returns:
-        List of integers corresponding to the symbols in the text
-    """
     sequence = []
 
     # Check for curly braces and treat their contents as ARPAbet:
